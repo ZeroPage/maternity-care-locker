f0581863dac932cbd5b3f05d29672f55dc153726
Indigobit <cho_hol@outlook.com>
2016-10-08 17:14:07 +0900

    feat(Advice): 임산부 조언 텍스트 뷰 추가

--- app/src/main/java/me/skywave/maternitycarelocker/locker/LockerCareController.java
+++ app/src/main/java/me/skywave/maternitycarelocker/locker/LockerCareController.java
@@ -1,11 +1,14 @@
 package me.skywave.maternitycarelocker.locker;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextClock;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import me.skywave.maternitycarelocker.R;
@@ -13,11 +16,23 @@ import me.zhanghai.android.patternlock.PatternView;
 
 public class LockerCareController {
     private View currentView;
-    private TextView unlockActionText;
 
     public LockerCareController(Context context) {
         LayoutInflater inflater = LayoutInflater.from(context);
         currentView = inflater.inflate(R.layout.view_care, null);
+
+        prepareTypeFaces(currentView, "NotoSansKR-Light.otf", context);
+    }
+
+    private void prepareTypeFaces(View view, String fontName, Context context) {
+        ArrayList<TextView> textViews = new ArrayList<>();
+        textViews.add((TextView) view.findViewById(R.id.adviceText));
+
+        Typeface type = Typeface.createFromAsset(context.getAssets(), fontName);
+
+        for (TextView textView : textViews) {
+            textView.setTypeface(type);
+        }
     }
 
     public View getView() {
