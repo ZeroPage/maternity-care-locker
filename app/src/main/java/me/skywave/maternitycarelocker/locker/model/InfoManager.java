697e79ac5e9f4362971c59cc8f3efffa0d65f675
Indigobit <cho_hol@outlook.com>
2016-10-15 17:17:47 +0900

    chore(Info): 임산부 정보 데이터 변경

--- app/src/main/java/me/skywave/maternitycarelocker/locker/model/InfoManager.java
+++ app/src/main/java/me/skywave/maternitycarelocker/locker/model/InfoManager.java
@@ -11,10 +11,10 @@ public class InfoManager {
     public static InfoVO getInfoVO() {
         if (info == null) {
             info = new InfoVO();
-            info.setName("안미리");
+            info.setName("임 산부");
             info.setBirth("1993.12.08");
-            info.setAllergies("과제 알레르기, 졸업논문 알레르기, 자소서 알레르기, 면접 알레르기");
-            info.setMedicines("감기약");
+            info.setAllergies("고양이 알레르기, 계란 알레르기, 땅콩 알레르기");
+            info.setMedicines("혈압약");
             info.addContactName("박희정");
             info.addContact("01047429665");
             info.addContactName("조영준");
