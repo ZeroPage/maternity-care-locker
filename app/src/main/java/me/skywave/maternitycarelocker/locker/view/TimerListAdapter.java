2e68e6a4860289fba538a7008f88656987ebbc39
kameru <kameru1208@gmail.com>
2016-10-16 18:14:22 +0900

    feat(Timer): 타이머 리스트 아이템 구현

---
+++ app/src/main/java/me/skywave/maternitycarelocker/locker/view/TimerListAdapter.java
@@ -0,0 +1,66 @@
+package me.skywave.maternitycarelocker.locker.view;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import me.skywave.maternitycarelocker.R;
+import me.skywave.maternitycarelocker.locker.model.TimerVO;
+
+public class TimerListAdapter extends BaseAdapter{
+    private Context context;
+    private ArrayList<TimerVO> timerList;
+    private int resource;
+
+    public TimerListAdapter(Context context, int resource, ArrayList<TimerVO> timerList) {
+        this.context = context;
+        this.resource = resource;
+        this.timerList = timerList;
+    }
+
+    @Override
+    public int getCount() {
+        return timerList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return timerList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (view == null) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            view = inflater.inflate(resource, viewGroup, false);
+        }
+
+        TextView typeText = (TextView) view.findViewById(R.id.typeText);
+        TextView periodText = (TextView) view.findViewById(R.id.periodText);
+        TextView startText = (TextView) view.findViewById(R.id.startText);
+
+        TimerVO timer = (TimerVO) getItem(i);
+        typeText.setText(timer.getType());
+        if (timer.getType().equals(TimerVO.TYPE_PAIN)) {
+            typeText.setTextColor(Color.RED);
+        }
+        else if (timer.getType().equals(TimerVO.TYPE_REST)) {
+            typeText.setTextColor(Color.BLUE);
+        }
+        periodText.setText(timer.getPreiod());
+        startText.setText(timer.getStart());
+
+        return view;
+    }
+}
