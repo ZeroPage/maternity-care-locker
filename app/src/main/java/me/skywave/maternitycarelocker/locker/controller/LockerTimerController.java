ded579b84920e384bf02c2209f7a6ecfdb792354
Indigobit <cho_hol@outlook.com>
2016-10-16 17:00:42 +0900

    feat(Timer): 타이머 기능 레이아웃

--- app/src/main/java/me/skywave/maternitycarelocker/locker/controller/LockerTimerController.java
+++ app/src/main/java/me/skywave/maternitycarelocker/locker/controller/LockerTimerController.java
@@ -1,6 +1,12 @@
 package me.skywave.maternitycarelocker.locker.controller;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import java.util.ArrayList;
 
 import me.skywave.maternitycarelocker.R;
 
@@ -9,9 +15,45 @@ public class LockerTimerController extends LockerController {
     public LockerTimerController(Context context) {
         super(R.layout.view_timer, context);
         update();
+        prepareTypeFaces();
+        prepareTimer();
     }
 
     public void update() {
 
+
+    }
+
+    private void prepareTimer() {
+        final ToggleButton startButton = (ToggleButton) currentView.findViewById(R.id.onOffButton);
+        final ToggleButton stopButton = (ToggleButton) currentView.findViewById(R.id.recordButton);
+        final ListView listView = (ListView) currentView.findViewById(R.id.timerListView);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(startButton.isChecked()) {
+                    stopButton.setVisibility(View.VISIBLE);
+                    stopButton.setChecked(true);
+                } else {
+                    stopButton.setVisibility(View.GONE);
+                }
+            }
+        });
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(stopButton.isChecked()) {
+
+                } else {
+                }
+            }
+        });
+    }
+
+    private void prepareTypeFaces() {
+        ArrayList<TextView> lightTexts = new ArrayList<>();
+        lightTexts.add((TextView) currentView.findViewById(R.id.timerText));
+
+        setTypeFaces(FONT_NOTO_THIN, lightTexts);
     }
 }
